0495c952c8138c4ad236e71aa61e4e2c72ab9c4b
Prerit <preritmishra1002@gmail.com>
2023-09-06 11:24:28 +0530

    Added function to compare incoming password with encrypted password

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const prepareAndStartServer = () => {
     app.use('/api', apiRoutes);
     app.listen(PORT, async() => {
         console.log(`Server started on Port: ${PORT}`);
-        const service = new UserService();
+        //const service = new UserService();
         // const newToken = service.createToken({email: 'prerit@admin.com', id: 1});
         // console.log("new token is ", newToken);
         // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InByZXJpdEBhZG1pbi5jb20iLCJpZCI6MSwiaWF0IjoxNjkzOTc4MzczLCJleHAiOjE2OTM5Nzg0MDN9.mcPAhQ3bn2uoHGabTaD-44PPLt2i98ed1s1Nv1UZqzw';
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const UserRepository = require('../repository/user-repository');
 const {JWT_KEY} = require('../config/serverConfig'); 
@@ -33,6 +34,15 @@ class UserService {
             return response;
         } catch (error) {
             console.log("Something went wrong in token validation", error);
+            throw {error};
+        }
+    }
+
+    checkPassword (userInputPassword, encryptedPassword){
+        try {
+            return bcrypt.compareSync(userInputPassword,encryptedPassword);
+        } catch (error) {
+            console.log("Something went wrong in password verification");
             throw {error};
         }
     }
