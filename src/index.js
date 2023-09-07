1079d9a3125cb460f5cca0ab04df08c8e8cc8b85
Prerit <preritmishra1002@gmail.com>
2023-09-07 16:10:02 +0530

    Added isAdmin api

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 
 const db = require('./models/index');
-const {User, Role} = require('./models/index');
+//const {User, Role} = require('./models/index');
 //const UserService = require('./services/user-service');
 
 const app = express();
@@ -21,18 +21,7 @@ const prepareAndStartServer = () => {
             db.sequelize.sync({alter:true});
         }
 
-        const u1 = await User.findByPk(4);
-        const r1  = await Role.findByPk(3);
-        //u1.addRole(r1);
-        const response = await u1.hasRole(r1);
-        console.log(response);
         
-        //const service = new UserService();
-        // const newToken = service.createToken({email: 'prerit@admin.com', id: 1});
-        // console.log("new token is ", newToken);
-        // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InByZXJpdEBhZG1pbi5jb20iLCJpZCI6MSwiaWF0IjoxNjkzOTc4MzczLCJleHAiOjE2OTM5Nzg0MDN9.mcPAhQ3bn2uoHGabTaD-44PPLt2i98ed1s1Nv1UZqzw';
-        // const response = service.verifyToken(token);
-        // console.log(response);
     });
 }
 
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -83,6 +83,16 @@ class UserService {
             throw {error};
         }
     }
+
+    isAdmin(userId) {
+        try {
+            return this.userRepository.isAdmin(userId);
+        } catch (error) {
+            console.log("Something went wrong in service layer");
+            throw {error};
+            
+        }
+    }
 }
 
 module.exports = UserService;
